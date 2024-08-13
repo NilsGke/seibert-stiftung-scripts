d1d96a6468338b61fe84ce09af2de9058883227f
NilsGke <n.j.goeke@gmail.com>
2024-08-13 23:22:48 +0200

    test script

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Seibert Stiftung Global
+// @namespace    https://programmieren.de
+// @description  general purpose script for Seibert Stiftung Chromebooks
+// @author       NilsGke
+// @match        *://*/*
+// @grant        none
+// @downloadURL  https://gist.github.com/NilsGke/60c82ac3926d0799576ea781a0324d2a/raw
+// @updateURL    https://gist.github.com/NilsGke/60c82ac3926d0799576ea781a0324d2a/raw
+// @version      {{version}}
+// ==/UserScript==
+
+console.log("%cHello World!", "color:lime;font-size:30px;background:red;");
